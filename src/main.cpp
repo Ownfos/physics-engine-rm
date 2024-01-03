5d1160e8ad752b8d556b051e885c7bf27bee671d
ownfos <mejhg1234@gmail.com>
2024-01-03 19:44:29 +0900

    Test drawing circle and convex polygon

--- src/main.cpp
+++ src/main.cpp
@@ -19,15 +19,41 @@ int main()
         }
 
         ImGui::SFML::Update(window, deltaClock.restart());
-        ImGui::ShowDemoWindow();
         ImGui::Begin("test window");
-        if (ImGui::Button("button"))
-        {
-            printf("haha");
-        }
+        static float x_offset = 0;
+        ImGui::SliderFloat("x offset", &x_offset, 0.0f, 100.0f);
+        static float rotation = 0;
+        ImGui::SliderFloat("rotation", &rotation, 0.0f, 360.0f);
         ImGui::End();
 
         window.clear(sf::Color::White);
+
+        // draw polygon
+        {
+            sf::ConvexShape triangle(3);
+            triangle.setFillColor(sf::Color::Black);
+
+            // define the position of the triangle's points
+            triangle.setPoint(0, {-50, -50});
+            triangle.setPoint(1, {50, -50});
+            triangle.setPoint(2, {50, 50});
+
+            triangle.rotate(rotation);
+            triangle.move(100 + x_offset, 100);
+            window.draw(triangle);
+        }
+
+        // draw circle
+        {
+            sf::CircleShape circle(4);
+            circle.setOrigin(4, 4);
+            circle.setFillColor(sf::Color::Red);
+            circle.rotate(rotation);
+            circle.move(100 + x_offset, 100);
+            window.draw(circle);
+        }
+
+
         ImGui::SFML::Render(window);
         window.display();
     }
