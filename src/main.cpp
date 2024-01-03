8b585afc6c6c19725f24dc1cdac217768677240a
ownfos <mejhg1234@gmail.com>
2024-01-03 19:17:16 +0900

    Test ImGui integration

--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,37 @@
 #include "SFML/Graphics.hpp"
-#include <iostream>
+#include "imgui.h"
+#include "imgui-SFML.h"
 
 int main()
 {
-    // create the window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+    sf::RenderWindow window(sf::VideoMode(800, 600), "physics!");
+    if (!ImGui::SFML::Init(window)) return -1;
 
-    // run the program as long as the window is open
+    sf::Clock deltaClock;
     while (window.isOpen())
     {
-        // check all the window's events that were triggered since the last iteration of the loop
         sf::Event event;
         while (window.pollEvent(event))
         {
-            // "close requested" event: we close the window
+            ImGui::SFML::ProcessEvent(window, event);
             if (event.type == sf::Event::Closed)
                 window.close();
         }
 
-        window.clear(sf::Color::White);
-
-        // draw everything here...
-        // window.draw(...);sf::Text text;
-        sf::Text text;
-        sf::Font font;
-        if (!font.loadFromFile("../../../fonts/arial.ttf"))
+        ImGui::SFML::Update(window, deltaClock.restart());
+        ImGui::ShowDemoWindow();
+        ImGui::Begin("test window");
+        if (ImGui::Button("button"))
         {
-            // error...
+            printf("haha");
         }
+        ImGui::End();
 
-        text.setFont(font); // font is a sf::Font
-
-        // set the string to display
-        text.setString("Hello world");
-
-        // set the character size
-        text.setCharacterSize(24); // in pixels, not points!
-
-        // set the color
-        text.setFillColor(sf::Color::Red);
-
-        // set the text style
-        text.setStyle(sf::Text::Bold | sf::Text::Underlined);
-
-        // inside the main loop, between window.clear() and window.display()
-        window.draw(text);
-
-        // end the current frame
+        window.clear(sf::Color::White);
+        ImGui::SFML::Render(window);
         window.display();
     }
+    ImGui::SFML::Shutdown();
 
     return 0;
 }
